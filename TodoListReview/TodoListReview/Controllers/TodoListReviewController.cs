c8f96709195bbc0c803de5c5f4f56fc68dfc3161
ZFan7 <zfan7@slb.com>
2020-11-01 10:14:52 +0800

    backend homeword copy

---
+++ TodoListReview/TodoListReview/Controllers/TodoListReviewController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoListReview.Models;
+using TodoListReview.Services;
+
+namespace TodoListReview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListReviewController : ControllerBase
+    {
+        private readonly ItemService _itemService;
+
+        public TodoListReviewController(ItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> Get()
+        {
+            var items = await _itemService.Get();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> Search(long id)
+        {
+            var item = await _itemService.Get(id);
+
+            if (item == null)
+            {
+                return NotFound($"No item match id: {id}");
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Item itemIn)
+        {
+            var item = await _itemService.Get(itemIn.Id);
+
+            if (item != null)
+            {
+                return BadRequest($"Item match id: {itemIn.Id} have existed");
+            }
+
+            await _itemService.Add(itemIn);
+
+            return Ok("Add success");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var item = await _itemService.Get(id);
+
+            if (item == null)
+            {
+                return NotFound($"No item match id: {id}");
+            }
+
+            await _itemService.Delete(id);
+
+            return Ok("Delete success");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Item>> Updete(long id, Item itemIn)
+        {
+            var item = await _itemService.Get(id);
+
+            if (item == null)
+            {
+                return NotFound($"No item match id: {id}");
+            }
+
+            await _itemService.Update(id, itemIn);
+
+            return Ok("Update success");
+        }
+    }
+}
---
+++ TodoListReview/TodoListReview/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using TodoListReview.Models;
+using TodoListReview.Services;
+
+namespace TodoListReview
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ItemSettings>(
+                Configuration.GetSection(nameof(ItemSettings)));
+            services.AddSingleton<IItemSettings>(
+                sp => sp.GetRequiredService<IOptions<ItemSettings>>().Value);
+
+            services.AddSingleton<ItemService>();
+
+            services.AddControllers();
+
+            // Swagger addition part 1
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                var folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var xmlFiles = Directory.GetFiles(folder, "*.xml").ToList();
+                xmlFiles.ForEach(x => c.IncludeXmlComments(x));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Swagger addition part 2
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Issue To Do List V1");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
