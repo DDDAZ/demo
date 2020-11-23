d209c1cdb811ac27e94612bbe69732466901ebf2
ZFan7 <zfan7@slb.com>
2020-11-23 14:59:20 +0800

    add: web api TW

---
+++ thoughtswork/HelloApi/HelloApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+
+        [HttpGet("weatherforecast")]
+        public WeatherForecast GetFirstData()
+        {
+            return new WeatherForecast();
+        }
+    }
+}
